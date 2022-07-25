8e9e8ab4f2ccb8011afb4698dca647f855ac47b2
Kelliton <programmerKelliton@gmail.com>
2022-07-25 10:41:14 -0300

    Adicionando nova tela

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Container from "../components/Container";
+import Thanks from "../components/Thanks";
 
 function App() {
     return (
         <div className="App">
-            <Container></Container>
+            {/* <Container></Container> */}
+            <Thanks></Thanks>
         </div>
     );
 }
---
+++ src/components/Thanks/index.jsx
@@ -0,0 +1,16 @@
+import "./style.scss";
+import Icon from "../../assets/icons/illustration-thank-you.svg";
+
+export default function Thanks() {
+    return (
+        <div className="Thanks">
+            <img className="Thanks__icon" src={Icon} alt="" />
+            <span className="Thanks__selectText">You selected X out of X</span>
+            <h1 className="Thanks__title">Thank you</h1>
+            <p className="Thanks__message">
+                We appreciate you taking the time to give a rating. If you ever
+                more suport, dont't hesitate to get in touch!
+            </p>
+        </div>
+    );
+}
